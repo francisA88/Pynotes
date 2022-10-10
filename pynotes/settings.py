28e0f293284f648598084ade8f958ea9e84769ed
Francis Ali <githubemail61@gmail.com>
2022-10-10 20:07:36 +0100

    changed settings.py

--- pynotes/settings.py
+++ pynotes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', "simplenotes.up.railway.app"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', "simplenotes.up.railway.app", ".up.railway.app"]
 
 
 # Application definition
